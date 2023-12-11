63cb5b0a7099805c1c1c23d8430d09c11a12acf4
Cerbermx <damianwodka98@gmail.com>
2023-12-11 10:20:13 +0100

    Commit V0.2

--- Documents/NetBeansProjects/GAME/src/game/GAMEPanel.java
+++ Documents/NetBeansProjects/GAME/src/game/GAMEPanel.java
@@ -4,8 +4,10 @@
  */
 package game;
 
+import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
 import javax.swing.JPanel;
 
 /**
@@ -18,8 +20,38 @@ public class GAMEPanel  extends JPanel implements ActionListener{
         
     }
     
+    public void startGame(){
+        
+    }
+    
+    public void paintComponent(Graphics g){
+        
+    }
+    
+    public void draw(Graphics g){
+        
+    }
+    
+    public void move(){
+        
+    }
+    
+    public void checkApple(){
+        
+    }
+    public void checkCollisions(){
+        
+       
+    }
+    
+    public void gameOver(Graphics g){
+        
+    }
+    
     public void actionPerformed(ActionEvent e){
         
     }
+    
+    public class MyKeyAdapter extends KeyAdapter{
     
 }
