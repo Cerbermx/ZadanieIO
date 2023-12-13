529d30953b350a2429f044673dcf63871aea8d85
przemekcfc90 <p.pietrajtys@gmail.com>
2023-12-13 22:32:15 +0100

    Commit V0.5

--- Documents/NetBeansProjects/GAME/src/game/GAMEPanel.java
+++ Documents/NetBeansProjects/GAME/src/game/GAMEPanel.java
@@ -16,8 +16,8 @@ import java.util.Random;
  */
 public class GAMEPanel  extends JPanel implements ActionListener{
     
-    static final int SCREEN_WIDTH = 900;
-    static final int SCREEN_HEIGHT = 900;
+    static final int SCREEN_WIDTH = 700;
+    static final int SCREEN_HEIGHT = 700;
     static final int UNIT_SIZE = 25;
     static final int GAME_UNITS = (SCREEN_WIDTH*SCREEN_HEIGHT)/UNIT_SIZE;
     static final int DELAY = 75;
@@ -58,8 +58,19 @@ public class GAMEPanel  extends JPanel implements ActionListener{
     
     public void draw(Graphics g){
         
+        for (int i=0;i<SCREEN_HEIGHT/UNIT_SIZE;i++){
+            g.drawLine(i*UNIT_SIZE,0,i*UNIT_SIZE,SCREEN_HEIGHT);
+            g.drawLine(0,i*UNIT_SIZE,SCREEN_HEIGHT,i*UNIT_SIZE);
+        }
+        g.setColor(Color.red);
+        g.fillOval(appleX,appleY,UNIT_SIZE,UNIT_SIZE);
+        
     }
-    
+    public void newApple(){
+        appleX = random.nextInt((int)(SCREEN_WIDTH/UNIT_SIZE))*UNIT_SIZE;
+        appleY = random.nextInt((int)(SCREEN_HEIGHT/UNIT_SIZE))*UNIT_SIZE;
+        
+    }
     public void move(){
         
     }
